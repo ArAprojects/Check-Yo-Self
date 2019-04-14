90057ae7030656c83d8cc42e70abbbe0b2006426
Alek <alekaker@outlook.com>
2019-04-14 16:27:38 -0600

    adding disable button state for new item list

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ var taskBodyInput = document.querySelector('.to-do-item-input')
 
 newItemButton.addEventListener('click', addNewListItem)
 newItemButton.addEventListener('click', resetInputs)
+taskBodyInput.addEventListener('keyup', checkListInputs)
+taskTitleInput.addEventListener('keyup', checkListInputs)
 
 
-function addNewListItem() {
-leftCheckList.innerHTML = `
+function addNewListItem(e) {
+  e.preventDefault()
+  leftCheckList.innerHTML = `
   <li class="list-item"><img class="delete-from-sidebar-button" src="assets/delete.svg">${taskBodyInput.value}</li>`
   + leftCheckList.innerHTML;
 }
@@ -17,4 +20,12 @@ leftCheckList.innerHTML = `
 function resetInputs(){
   taskTitleInput.value = ''
   taskBodyInput.value = ''
+}
+
+function checkListInputs() {
+  if (taskTitleInput.value === "" || taskBodyInput.value === "") {
+    newItemButton.disabled = true;
+  } else {
+    newItemButton.disabled = false;
+  }
 }
